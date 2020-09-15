277a169e27aa2dde2b1e5a04afe6917e3932e9a9
Angel Ho <angel.ho@ryerson.ca>
2020-09-15 15:48:37 -0400

    AR use of .sum, .avg, .where and .count to fetch data

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#output total revenue for all the stores using .sum
+puts Store.sum(:annual_revenue)
+
+#output the avg annual revenue for all stores
+puts Store.average(:annual_revenue)
+
+#output the number of stores that are > 1M using where size or count
+puts Store.where("annual_revenue < ?", 1000000).count
