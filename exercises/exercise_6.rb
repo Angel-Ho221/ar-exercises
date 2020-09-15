916010b6d80e57e918e6d33f2998deb5435fd7a5
Angel Ho <angel.ho@ryerson.ca>
2020-09-15 15:58:23 -0400

    added employee insert info using belongs to store and has many employees, added random database in exercise 6 complete.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Angel", last_name: "Ho", hourly_rate: 100)
+@store2.employees.create(first_name: "Tracer", last_name: "Tang", hourly_rate: 50)
+@store2.employees.create(first_name: "Genji", last_name: "Ho", hourly_rate: 48)
+@store1.employees.create(first_name: "Belle", last_name: "Tang-Ho", hourly_rate: 500)
+@store1.employees.create(first_name: "Hanzo", last_name: "Tang", hourly_rate: 48)
