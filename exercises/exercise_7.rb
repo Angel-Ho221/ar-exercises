39b74816c46ead235185a08e8ed76cdb5e7132ef
Angel Ho <angel.ho@ryerson.ca>
2020-09-15 16:02:57 -0400

    and added exercise 7 entering store name and revenue

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name:"
+@name = gets.chomp
+puts "Enter store revenue:"
+@annual_revenue = gets.chomp
+create_store = Store.create(name: @name, 
+annual_revenue: @annual_revenue
+)
+
+puts create_store.errors.messages
